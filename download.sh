296a46dd93d3f1b899b4a6185356185861f0d497
hilbert25 <hht@pku.edu.cn>
2019-02-19 11:04:42 +0800

    fix bugs

--- download.sh
+++ download.sh
@@ -6,11 +6,11 @@ git clone git@github.com:btcsuite/goleveldb.git
 git clone git@github.com:btcsuite/websocket.git
 git clone git@github.com:btcsuite/snappy-go.git
 cd ..
-git clone git@github.com:davecgh/go-spew.git davecgh/go-spew/spew
+git clone git@github.com:davecgh/go-spew.git davecgh/go-spew
 git clone git@github.com:jessevdk/go-flags.git jessevdk/go-flags
 git clone git@github.com:kkdai/bstream.git kkdai/bstream
 git clone git@github.com:aead/siphash.git aead/siphash
-mkdir -p jrick/logrotate/rotator
+git clone git@github.com:jrick/logrotate.git jrick/logrotate
 cd ..
 mkdir -p golang.org/x && cd golang.org/x
 git clone git@github.com:golang/crypto.git
