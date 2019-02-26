3a9a3d5f7f0380c6c14590dc9de98563df14e57b
hilbert25 <hht@pku.edu.cn>
2019-02-25 18:59:27 -0800

    add winsvc

--- download.sh
+++ download.sh
@@ -5,6 +5,7 @@ git clone git@github.com:btcsuite/go-socks.git
 git clone git@github.com:btcsuite/goleveldb.git
 git clone git@github.com:btcsuite/websocket.git
 git clone git@github.com:btcsuite/snappy-go.git
+git clone git@github.com:btcsuite/winsvc.git
 cd ..
 git clone git@github.com:davecgh/go-spew.git davecgh/go-spew
 git clone git@github.com:jessevdk/go-flags.git jessevdk/go-flags
